26d902ae0086fc4d160b71e7848479b70e450dcc
ahmad <ahsai001@gmail.com>
2024-06-25 23:20:08 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:portfolio/firebase_options.dart';
 import 'package:portfolio/src/app.dart';
 
-void main() {
+void main() async {
   usePathUrlStrategy();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
